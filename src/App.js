baff35b2aa22c5368a68b4dc41ad30654ba3c001
amarmanhala <amar.manhala@gmail.com>
2021-04-13 14:53:27 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <header>
+          <h1>Timely</h1>
+          <p className="secondary">Do tasks with in time slot.</p>
+        </header>
+        <div className="grid">
+          <input type={Text} autoFocus></input>
+          <div style={{ display: 'grid', gridTemplateColumns: 'auto auto auto', gridColumnGap: '0.8rem' }}>
+          <button className="btnSecondary">10 min</button>
+
+            <button className="btnSecondary">30 min</button>
+            <button className="btnSecondary">1 hour</button>
+          </div>
+          
+
+          <button className="addButton" disabled={true}>Start task</button>
+        </div>
+      </main>
     </div>
   );
 }
